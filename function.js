b23a80b1b7d7f18a5a8f5e6f911567528d5d7e49
MistyIslam5 <mdmisty5@gmail.com>
2024-09-05 20:59:31 +0600

    js function lerning

---
+++ function.js
@@ -0,0 +1,28 @@
+const price = 755;
+
+// 
+
+
+function toggleButton(){
+    console.log(price)
+}
+
+// toggleButton();
+
+
+function macherTorkary (mach, sobji, mashla){
+    console.log('tel Gorom korun');
+    console.log(mashla + 'Moshla Furan');
+    console.log(sobji + 'Shaksobji din');
+    console.log(mach  + 'din');
+
+
+    // ranna ses hole value return hobe
+
+    return 'sushadu maser torkari prostut';
+}
+
+const result = macherTorkary ('Ilish', 'alu', 'holud');
+
+console.log(result);
+
